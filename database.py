353f3a44c71618ddee6a5481a8b43c61548e8fc9
Jeremiah Lee <jeremiah.lee@gmail.com>
2014-04-12 09:21:12 -0700

    Making the commit process easier.

--- database.py
+++ database.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python
 
-# This code was written to solve the Thumbtack coding challenge up at http://www.thumbtack.com/challenges.
+# This code was written to solve the Thumbtack coding challenge up at
+# http://www.thumbtack.com/challenges.
 # Written by Jeremiah Lee, no warranty, yadda yadda.
 #
-# The idea behind the solution is that we will have a Context which stores a stack of Frame objects.
-# Each Frame object has hashes to store the key/value pairs of the database as well as a separate
+# The idea behind the solution is that we will have a Context which
+# stores a stack of Frame objects. Each Frame object has hashes to
+# store the key/value pairs of the database as well as a separate
 # hash for the number of each of the values currently being stored.
 #
-# The set, get, and unset operations occur on the current frame when they are performed. numequalto is a
-# special case because for that count to be correct, it is needed to examine the frames lower in the stack.
-# There is code below to handle that, but that is the reason that each of the Frame objects has a reference
-# to the overall context.
+# The set, get, and unset operations occur on the current frame when
+# they are performed. numequalto is a special case because for that
+# count to be correct, it is needed to examine the frames lower in
+# the stack. There is code below to handle that, but that is the
+# reason that each of the Frame objects has a reference to the
+# overall context.
 #
-# The transactional part of the database is actually fairly simple; when we start a transaction, we just overlay
-# a new Frame by pushing it onto the stack of Frame objects that the context is keeping. When we roll it back, we
-# pop the stack. Committing is a little more intense, causing use to walk the stack (all but the bottom Frame), applying
-# any changes that have not already been applied from "higher" Frames.
+# The transactional part of the database is actually fairly simple;
+# when we start a transaction, we just overlay a new Frame by pushing
+# it onto the stack of Frame objects that the context is keeping.
+# When we roll it back, we pop the stack. Committing is a little
+# more intense, causing use to walk the stack (all but the bottom
+# Frame), applying any changes that have not already been applied
+# from "higher" Frames.
 #
-# The main pain in this code is the numequalto operation because it needs to know whether a given key was used lower on
-# the stack. This pain is encapsulated in the decrease method. I chose not to pass the context directly to that method
-# because I did not want the API to have a 1 call take the context but not the rest of them. That is why I made it part
-# of the Frame construction.
+# The main pain in this code is the numequalto operation because
+# it needs to know whether a given key was used lower on the stack.
+# This pain is encapsulated in the decrease method. I chose not to
+# pass the context directly to that method because I did not want
+# the API to have a 1 call take the context but not the rest of
+# them. That is why I made it part of the Frame construction.
 #
-# One note about running this code, you can either feed it a file via STDIN to see the results or run the script
-# and it will be interactive; as you type commands, you will see results. This is on purpose. :-D
+# One note about running this code, you can either feed it a file
+# via STDIN to see the results or run the script and it will be
+# interactive; as you type commands, you will see results. This
+# is on purpose. :-D
 
 import sys
 import re
@@ -48,7 +59,10 @@ class Frame:
         if var_name in self.data:
             value = self.data[var_name]
 
-            self.numbers[ value ] = self.numbers.get(value, 0) - 1
+            if value not in self.numbers:
+                raise Exception
+
+            self.numbers[ value ] = self.numbers[value] - 1
 
             return
 
@@ -143,8 +157,10 @@ class Context:
     # basic methodology of the commit:
     # -walk the frames in reverse, excepting frames[0] (base_frame)
     # -for each of these, as long as the key name has not already been seen:
-    #   -examine all of the unset variables first, unsetting them in the base_frame, marking them as seen
-    #   -examine all of the keys in the current frame, setting them in the base_frame, marking them as seen
+    #   -examine all of the unset variables first, unsetting them in
+    #    the base_frame, marking them as seen
+    #   -examine all of the keys in the current frame, setting them in the
+    #    base_frame, marking them as seen
     # -remove the currently last frame from the stack of frames.
 
     def commit(self):
@@ -152,25 +168,16 @@ class Context:
             print "INVALID COMMIT - NO TRANSACTION(S) IN PROGRESS"
             return
 
-        seen = set()
+        # we are creating a new context entirely
+        new_context = Context()
+        new_base = Frame(new_context)
 
-        base_frame = self.frames[0]
-
-        for frame in reversed(self.frames[1:]):
-            for deleted in frame.get_all_deleted():
-                if deleted in seen:
-                    continue
-                base_frame.unset(deleted)
-                seen.add(deleted)
-
+        for frame in self.frames:
             for key in frame.keys():
-                if key in seen:
-                    continue
-                
-                base_frame.set(key, frame.get(key))
-                seen.add(key)
+                new_base.set(key, frame.get(key))
 
-            self.frames.pop()
+        # and replacing the current context with the new context
+        self = new_context
 
     def call(self, args):
         func_name = args[0].lower()
